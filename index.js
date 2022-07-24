b008b54004a7ab02a5bf551768a6c67a96117bbf
Bruce Liu <yiyubruceliu@gmail.com>
2022-07-24 17:59:28 +0200

    changes

--- index.js
+++ index.js
@@ -15,6 +15,35 @@ import bootstrap from "bootstrap";
 const superfish = require("/js/superfish.min.js");
 const magnific = require("/js/jquery.magnific-popup.min.js");
 const carousel = require("/js/owl.carousel.min.js");
+//Init the carousel
+initSlider();
+
+function initSlider() {
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    onInitialized: startProgressBar,
+    onTranslate: resetProgressBar,
+    onTranslated: startProgressBar,
+  });
+}
+
+function startProgressBar() {
+  // apply keyframe animation
+  $(".slide-progress").css({
+    width: "100%",
+    transition: "width 5000ms"
+  });
+}
+
+function resetProgressBar() {
+  $(".slide-progress").css({
+    width: 0,
+    transition: "width 0s"
+  });
+}
+
 // const isotope = require("./js/isotope.pkgd.min.js");
 const Typed = require("/js/typed.min.js");
 var typed = new Typed("#typed", {
