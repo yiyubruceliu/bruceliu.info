97911f1fb0a9319aaa31b8d71b98a7f2be7e9611
Bruce Liu <yiyubruceliu@gmail.com>
2022-08-29 20:56:34 +0200

    changes

--- js/custom.js
+++ js/custom.js
@@ -213,21 +213,22 @@ $(document).ready(function() {
 
 $("#btnSendMail").on("click", async function (){
   console.log("hitting the send button");
-  var name = $("#msgName").val();
-  var subject = $("#msgSubject").val();
-  var email = $("#msgEmail").val();
-  var message = $("#msgMessage").val();
 
-  //sendEmail(name,subject, email, message);
+  // var name = $("#msgName").val();
+  // var subject = $("#msgSubject").val();
+  // var email = $("#msgEmail").val();
+  // var message = $("#msgMessage").val();
+
+  // //sendEmail(name,subject, email, message);
   
-  const payload = {
-    "name": name,
-    "subject": `bruceliu.info: ${subject}`, 
-    "email": email,
-    "message": message,
-    "password": "alshsniho092ujei489h"
-  } ;
-  console.log(payload);
+  // const payload = {
+  //   "name": name,
+  //   "subject": `bruceliu.info: ${subject}`, 
+  //   "email": email,
+  //   "message": message,
+  //   "password": "alshsniho092ujei489h"
+  // } ;
+  // console.log(payload);
   // const response = await fetch('https://bruceliu-info-email-service.herokuapp.com/sendEmail', {
   //   method: 'post',
   //   body: JSON.stringify(payload),
