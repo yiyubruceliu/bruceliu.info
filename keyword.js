bdc5cc0f7769de775061f8bf99ffe5f5fedf8532
Bruce Liu <yiyubruceliu@gmail.com>
2022-12-23 11:52:07 +0100

    changes

--- keyword.js
+++ keyword.js
@@ -1,31 +1,59 @@
-// import $ from "jquery";
-// const jQuery = require("./js/jquery-2.2.4.min.js");
-
-import item from 'outlayer/item';
-
-// const $ = require("./lib/jquery/jquery.min.js");
 var natural = require('natural');
 window.natural = natural;
 
+function filterStopWordsAndSymbols(words) {
+    const stopWords = natural.stopwords;
+    let keywords = words.filter(word => {
+        return !stopWords.includes(word) && word.length > 3 && /^[a-zA-Z]+$/.test(word);
+    });
+    return keywords;
+}
+
+function getUniqueBigramList(ngrammedKeywords) {
+    uniqueBiGrams = [];
+    ngrammedKeywords.map(bigram => {
+        let exists = false;
+        bigram.map(keyword => {
+            exists = uniqueBiGrams.some(innerArray => innerArray.includes(keyword));
+        });
+
+        if (!exists) {
+            uniqueBiGrams.push(bigram);
+        }
+    });
+
+    return uniqueBiGrams;
+}
+
 export function getSEOKeywords(text) {
-    
+    console.log("new changes");
     // Split the text into individual words
-    const words = text.split(' ');
-    const stemmer = natural.PorterStemmer;            
+    const words = text.split(' ');          
     
     // Remove common words and words that are less than a certain length
-    let keywords = words.filter(word => {
-        const stopWords = natural.stopwords;
-        return !stopWords.includes(word) && word.length > 3  && /^[a-zA-Z]+$/.test(word);
-    });
-    console.log(keywords);
-    //stem the text
-    stemmedKeywords = keywords.map(keyword => stemmer.stem(keyword));
+    let keywords = filterStopWordsAndSymbols(words);
 
+    //creating bi-grams
     ngrammedKeywords = natural.NGrams.ngrams(keywords, 2);
-    console.log(ngrammedKeywords);
 
-    // Count the number of times each keyword appears in the text
+        // Count the number of times each keyword appears in the text
+    const keywordCounts = countKeywords();
+
+    // Sort the keywords by the number of times they appear in the text
+    const sortedKeywords = ngrammedKeywords.sort((a, b) => keywordCounts[b] - keywordCounts[a]);
+
+    // Use a Set object to remove duplicates from the sorted keywords
+    const uniqueKeywords = getUniqueBigramList(sortedKeywords);
+    console.log("uniquekeywords >>", uniqueKeywords);
+    
+    // Return the most frequently used keywords
+    return Array.from(uniqueKeywords).slice(0, 10).map(item=>{
+        return `${item[0]} ${item[1]}`;
+    });
+}
+
+
+function countKeywords() {
     const keywordCounts = {};
     ngrammedKeywords.forEach(keyword => {
         if (keywordCounts[keyword]) {
@@ -34,24 +62,13 @@ export function getSEOKeywords(text) {
             keywordCounts[keyword] = 1;
         }
     });
-
-    // Sort the keywords by the number of times they appear in the text
-    const sortedKeywords = ngrammedKeywords.sort((a, b) => keywordCounts[b] - keywordCounts[a]);
-
-    // Use a Set object to remove duplicates from the sorted keywords
-    const uniqueKeywords = new Set(sortedKeywords);
-
-    // Return the most frequently used keywords
-    return Array.from(uniqueKeywords).slice(0, 10).map(item=>{
-        return `${item[0]} ${item[1]}`;
-    });
+    return keywordCounts;
 }
 
 export function findKeyword(){
     //input
     let text = 'lazy The quick brown fox jumps over the lazy dog';
     text = document.getElementById("msgBody").value;
-
     //process
     const keywords = getSEOKeywords(text);
 
