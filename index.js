083b6e5fe18e88bd2bcaf40cd9c71e659b363314
Bruce Liu <yiyubruceliu@gmail.com>
2023-01-03 10:25:22 +0100

    changes

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const superfish = require("/js/superfish.min.js");
 const magnific = require("/js/jquery.magnific-popup.min.js");
 const carousel = require("/js/owl.carousel.min.js");
 
-import { findKeyword } from "./keyword.js";
+//import { findKeyword } from "./keyword.js";
 import { getImage } from "./image.js";
 
 //Init the carousel
